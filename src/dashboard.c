356bdfa6feb4f83a3d2df1dc875989136ba05ae0
DevAriG <arielacostagarcia@gmail.com>
2026-09-16 17:53:29 -0400

    Read real uptime

--- src/dashboard.c
+++ src/dashboard.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include "dashboard.h"
 
+static unsigned long read_uptime(void) {
+    FILE *fp = fopen("/proc/uptime", "r");
+    if (!fp) {
+        return 0;
+    }
+
+    unsigned long seconds = 0;
+    fscanf(fp, "%lu", &seconds);
+    fclose(fp);
+
+    return seconds;
+}
+
 void dashboard_run(const SystemInfo *sys_info) {
     printf("ARIEL SYSTEM MONITOR\n");
     printf("--------------------\n\n");
@@ -15,7 +28,7 @@ SystemInfo system_information() {
     sys_info.cpu_usage = 45.5;
     sys_info.ram_usage = 65.2;
     sys_info.disk_usage = 80.1;
-    sys_info.uptime = 123456;
+    sys_info.uptime = read_uptime();
 
     return sys_info;
 }
